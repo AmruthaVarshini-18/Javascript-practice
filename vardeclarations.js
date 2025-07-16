5f8eda5720fc6342ab9f07899aa6679b94f6efa0
AmruthaVarshini-18 <22a31a4463@pragati.ac.in>
2025-07-16 13:51:45 +0530

    Add files via upload

---
+++ vardeclarations.js
@@ -0,0 +1,17 @@
+let name = "Amrutha";
+console.log(name);
+name = "Ammu";
+console.log(name);
+const age = 20;
+console.log(age);
+let isFollow = true;
+console.log(isFollow);
+{
+  let a = 5;
+
+  console.log(a);
+}
+{
+  let a = 10;
+  console.log(a);
+}
