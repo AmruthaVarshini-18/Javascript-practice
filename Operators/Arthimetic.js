94a662951363c80263c2d9326557e9a2ea425f53
AmruthaVarshini-18 <22a31a4463@pragati.ac.in>
2025-07-17 11:31:43 +0530

    Arthimetic.js

---
+++ Operators/Arthimetic.js
@@ -0,0 +1,35 @@
+//Arthimetic Operators
+let a = 10;
+let b = 8;
+//1.Addition(+)
+console.log("a+b=", a + b);
+//2.Subtraction(-)
+console.log("a-b=", a - b);
+//3.Multiplication(*)
+console.log("a*b=", a * b);
+//4.Division(/)
+console.log("a/b=", a / b);
+//5.Modulus(%)
+console.log("a%b=", a % b);
+//6.Exponentation(**)
+console.log("a**b=", a ** b);
+
+//Unary Operators
+//1.Increement(++)
+//pre increement
+console.log("a=", a);
+console.log("++a=", ++a);
+console.log("a=", a);
+//post increement
+console.log("a=", a);
+console.log("a++=", a++);
+console.log("a=", a);
+//2.Decreement
+//predecreement
+console.log("a=", a);
+console.log("--a=", --a);
+console.log("a=", a);
+//postdecreement
+console.log("a=", a);
+console.log("a--=", a--);
+console.log("a=", a);
