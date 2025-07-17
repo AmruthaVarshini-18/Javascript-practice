024cd2c786d74df84e81479da68bb2ab98681fb5
AmruthaVarshini-18 <22a31a4463@pragati.ac.in>
2025-07-17 14:13:55 +0530

    if.js

---
+++ ConditionalStatements/if.js
@@ -0,0 +1,11 @@
+//conditional Statements
+//If statement
+let age = prompt(
+  "Enter your age and get to know you are eligible for voting or not!!!"
+);
+if (age >= 18) {
+  console.log("Eligible to vote");
+}
+if (age < 18) {
+  console.log("Not Eligible to voting");
+}
