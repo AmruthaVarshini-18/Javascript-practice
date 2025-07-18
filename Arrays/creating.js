03ca1a393b7e4a333b05b06c5f9781cde9b5ea4f
AmruthaVarshini-18 <22a31a4463@pragati.ac.in>
2025-07-18 17:54:04 +0530

    Creating.js

---
+++ Arrays/creating.js
@@ -0,0 +1,14 @@
+//creating arrays
+let names = ["Amrutha", "varshini", "Ammu", "Varshu"];
+let marks = [96, 98, 87, 67];
+
+//finding lengths of arrays
+console.log(marks.length);
+console.log(names.length);
+//data type of array
+console.log(typeof marks);
+
+//Accessing array elements
+console.log(marks[0]);
+console.log(names[3]);
+console.log(marks[100]);
